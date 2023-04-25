8289e6d747a05d2a4ee25e707d63ae5e30826f3e
amsalugetasew <amsalugetasew12@gmail.com>
2023-04-25 04:23:04 -0700

    Commit Our printf project

--- main_file/main.h
+++ main_file/main.h
@@ -1,12 +1,58 @@
-#include "main.h"
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <stdio.h>
 
 /**
- * main - Entry point
+ *struct print - structure for different types of cases
+ * struct print - is the struct for printer functions
+ * @f: is the pointer to a printer functions
+ * @oper: is the identifier
  *
- * Return: Always 0
+ * Description: struct stores pointers to the
+ * printer functions.
  */
-int main(void)
+typedef struct print
 {
-    _printf("%S\n", "Best\nSchool");
-    return (0);
-}
+	char *oper;
+	int (*f)(va_list y);
+} print_t;
+
+/* General Prototypes */
+
+int _printf(const char *format, ...);
+int print_func(int *i, const char *format, va_list arguments);
+
+/* Putchar */
+int _putchar(char c);
+
+/* Main Prototypes*/
+
+int oper_char(va_list y);
+int oper_string(va_list y);
+int oper_percent(va_list y);
+int oper_numbers(va_list arg);
+
+/* Advanced Prototypes*/
+
+int oper_unsigned(va_list arg);
+int oper_binary(va_list y);
+int oper_octal(va_list y);
+int rot13(va_list y);
+int print_rev(va_list y);
+int printnumber(int n);
+int print_hex_lower(va_list arg);
+int print_hex_upper(va_list arg);
+int _strlen(char *s);
+/* print string without va_list */
+void _puts(char *s);
+char *convert_num_to_base(char range[], unsigned int num, int base);
+int print_S(va_list list);
+unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
+int print_buf(char *buf, unsigned int nbuf);
+
+#endif
